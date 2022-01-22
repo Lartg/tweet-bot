3407c0cbfe8b6313873f78b19f564049ae7e91a2
Lartg <roc.gerrety@gmail.com>
2022-01-21 17:14:42 -0800

    added some code review pointers

--- dictionary_words.py
+++ dictionary_words.py
@@ -2,13 +2,16 @@ import random, sys
 
 corpus = open('/usr/share/dict/words', 'r')
 dictionary = corpus.readlines()
+corpus.close()
+
 words = []
 
-for x in range(int(sys.argv[1])):
-  index = random.randint(0, len(dictionary)-1)
-  word = dictionary[index].strip('\n')
-  words.append(word)
+if __name__ == '__main__':
+  number = int(sys.argv[1]) or random.randint(len(dictionary)-1)
+  
+  for x in range(number):
+    index = random.randint(0, len(dictionary)-1)
+    word = dictionary[index].strip('\n')
+    words.append(word)
 
-
-
-print(*words, ' ')
+  print(*words, ' ')

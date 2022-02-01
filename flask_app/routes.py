f6e6e7ea30b981fee2c5f86d8fa00199c150d7f3
Lartg <roc.gerrety@gmail.com>
2022-02-01 09:21:43 -0800

    export random word

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,8 +1,13 @@
 from flask import Flask, Blueprint, render_template
-
+from sample import sample_word
+from histogram import histogram
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home_page():
-  """Return homepage."""
-  return render_template('base.html')
+  corpus = None
+  with open('text.txt', 'r') as c:
+    corpus = c.read()
+  
+  
+  return render_template('base.html', word=sample_word(histogram(corpus)[0]))

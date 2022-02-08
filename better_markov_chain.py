030a20976c8a431b8b65a154be7f1c871046fd31
Lartg <roc.gerrety@gmail.com>
2022-02-07 21:25:51 -0800

    made safer

--- better_markov_chain.py
+++ better_markov_chain.py
@@ -1,9 +1,11 @@
+from listogram import Listogram
 from text_to_list import text_to_list
 import random
 corpus = text_to_list('text.txt')
 char_limit = random.randint(50,100)
 tweet = ''
 corpus_length = len(corpus)
+listogram = Listogram(corpus)
 key = random.choice(corpus)
 tweet += f'{key} '
 while len(tweet) <= char_limit:
@@ -12,7 +14,11 @@ while len(tweet) <= char_limit:
     if key == same_word and index != corpus_length-1:
       pointer = index + 1
       new_word = corpus[pointer]
-      word_list.append(new_word)  
-  tweet += f'{random.choice(word_list)} '
+      word_list.append(new_word)
+  potential_key = random.choice(word_list)
+  
+  key = potential_key
+  tweet += f'{key} '
+
 print(tweet)
 

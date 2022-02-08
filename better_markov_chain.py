7698e3d1f1cc3b7d4513901563c582da2e8fdb93
Lartg <roc.gerrety@gmail.com>
2022-02-08 12:18:15 -0800

    planned improvements

--- better_markov_chain.py
+++ better_markov_chain.py
@@ -7,18 +7,23 @@ tweet = ''
 corpus_length = len(corpus)
 listogram = Listogram(corpus)
 key = random.choice(corpus)
-tweet += f'{key} '
+tweet += f'{key}'
 while len(tweet) <= char_limit:
   word_list = []
   for index, same_word in enumerate(corpus):
+    # enumerate in listogram construction 
+    # => eliminate loop as search occurs in construction 
+    # => knowing all indeces of potential chains
+    # still need a loop, but length is reduced significantly
     if key == same_word and index != corpus_length-1:
       pointer = index + 1
       new_word = corpus[pointer]
       word_list.append(new_word)
   potential_key = random.choice(word_list)
-  
+  # add a check here for the key 
+  # to generate more coherent sentences
   key = potential_key
-  tweet += f'{key} '
+  tweet += f' {key}'
 
 print(tweet)
 

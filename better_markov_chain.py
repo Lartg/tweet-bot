6112b89f60c80abf6fd37c7b54f4ab90625e0026
Lartg <roc.gerrety@gmail.com>
2022-02-07 20:58:45 -0800

    made things faster

--- better_markov_chain.py
+++ better_markov_chain.py
@@ -1,25 +1,18 @@
-from listogram import Listogram
 from text_to_list import text_to_list
 import random
-#corpus = ['one', 'fish', 'two', 'fish', 'red', 'fish', 'blue', 'fish', 'two', 'fish']
 corpus = text_to_list('text.txt')
-char_limit = 50
+char_limit = random.randint(50,100)
 tweet = ''
-
-listogram = Listogram(corpus)
-key = listogram.sample()
+corpus_length = len(corpus)
+key = random.choice(corpus)
 tweet += f'{key} '
-
 while len(tweet) <= char_limit:
-  corpus_index = 0
   word_list = []
-  for same_word in corpus:
-    if key == same_word and corpus_index != listogram.tokens -1:
-      pointer = corpus_index + 1
+  for index, same_word in enumerate(corpus):
+    if key == same_word and index != corpus_length-1:
+      pointer = index + 1
       new_word = corpus[pointer]
-      word_list.append(new_word)
-    corpus_index += 1
+      word_list.append(new_word)  
   tweet += f'{random.choice(word_list)} '
-
 print(tweet)
 

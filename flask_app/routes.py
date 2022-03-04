574eef646222c163888ac25d58005ad82ca9850f
Lartg <roc.gerrety@gmail.com>
2022-03-03 18:23:04 -1000

    integrated twitter api

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,4 +1,5 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request, redirect
+import twitter
 main = Blueprint('main', __name__)
 from even_better_markov_chain import generate_tweet
 
@@ -6,3 +7,10 @@ from even_better_markov_chain import generate_tweet
 def home_page():
   tweet = generate_tweet()
   return render_template('base.html', tweet=tweet)
+
+@main.route('/tweet', methods=['POST'])
+def tweet():
+  new_tweet = request.form['tweet']
+  print(new_tweet)
+  twitter.tweet(new_tweet)
+  return redirect('/')
